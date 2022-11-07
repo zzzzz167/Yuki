6cdcc1ec07c4e0cf96ca1e79e79ee508d8ed2458
zzzzz167 <2742400566@qq.com>
2022-11-07 10:25:28 +0800

    让杀软东西正常点

--- cores/backend.py
+++ cores/backend.py
@@ -17,6 +17,11 @@ import time
 
 saya = Saya.current()
 channel = Channel.current()
+restable = {
+    "sign": {"function": reset_sign, "name": "签到"},
+    "ban": {"function": resetBanList, "name": "黑名单"},
+    "tarot": {"function": reset_tarot, "name": "塔罗牌"},
+}
 
 
 @channel.use(ConsoleSchema([Twilight([FullMatch("stop")])]))
@@ -32,41 +37,27 @@ async def stop(app: Ariadne, console: Console):
 
 @channel.use(ConsoleSchema([Twilight.from_command("rest {module}")]))
 async def restModule(console: Console, module: RegexResult):
-    if str(module.result) == "sign":
+    if str(module.result) in restable.keys():
         res: str = await console.prompt(
             l_prompt=[("class:warn", " 确定重置%s? " % (module.result)), ("", " (y/n) ")],
             style=Style([("warn", "fg:#cdb4db")]),
         )
         if res.lower() in ("y", "yes"):
-            await reset_sign()
-            logger.info("签到重置成功")
-    elif str(module.result) == "ban":
-        res: str = await console.prompt(
-            l_prompt=[("class:warn", " 确定重置%s? " % (module.result)), ("", " (y/n) ")],
-            style=Style([("warn", "fg:#cdb4db")]),
-        )
-        if res.lower() in ("y", "yes"):
-            await resetBanList()
-            logger.info("黑名单重置成功")
-    elif str(module.result) == "tarot":
-        res: str = await console.prompt(
-            l_prompt=[("class:warn", " 确定重置%s? " % (module.result)), ("", " (y/n) ")],
-            style=Style([("warn", "fg:#cdb4db")]),
-        )
-        if res.lower() in ("y", "yes"):
-            await reset_tarot()
-            logger.info("每日塔罗重置成功")
+            await restable[module.result.display]["function"]()
+            logger.info(f"{restable[module.result.display]['name']}重置成功")
+    else:
+        logger.warning(f"模块{module.result.display}不存在")
 
 
-@channel.use(ConsoleSchema([Twilight.from_command("unban {id}")]))
-async def unBan(console: Console, id: RegexResult):
+@channel.use(ConsoleSchema([Twilight.from_command("unban {qqID}")]))
+async def unBan(console: Console, qqID: RegexResult):
     res: str = await console.prompt(
-        l_prompt=[("class:warn", " 确定解封%s? " % (id.result)), ("", " (y/n) ")],
+        l_prompt=[("class:warn", " 确定解封%s? " % (qqID.result)), ("", " (y/n) ")],
         style=Style([("warn", "fg:#cdb4db")]),
     )
     if res.lower() in ("y", "yes"):
-        await delBanList(int(str(id.result)))
-        logger.info("成功解禁%s" % str(id.result))
+        await delBanList(int(str(qqID.result)))
+        logger.info("成功解禁%s" % str(qqID.result))
 
 
 @channel.use(
@@ -74,26 +65,28 @@ async def unBan(console: Console, id: RegexResult):
         [
             Twilight(
                 FullMatch("ban"),
-                ParamMatch().space(SpacePolicy.FORCE) @ "id",
+                ParamMatch().space(SpacePolicy.FORCE) @ "qqID",
                 ParamMatch() @ "days",
                 WildcardMatch(optional=True) @ "tip",
             )
         ]
     )
 )
-async def Ban(console: Console, id: RegexResult, days: RegexResult, tip: RegexResult):
+async def Ban(console: Console, qqID: RegexResult, days: RegexResult, tip: RegexResult):
     res: str = await console.prompt(
-        l_prompt=[("class:warn", " 确定封禁%s? " % (id.result)), ("", " (y/n) ")],
+        l_prompt=[("class:warn", " 确定封禁%s? " % (qqID.result)), ("", " (y/n) ")],
         style=Style([("warn", "fg:#cdb4db")]),
     )
     if res.lower() in ("y", "yes"):
         if str(tip.result) == "":
-            await addBanList(int(str(id.result)), int(str(days.result)), time.time())
+            await addBanList(int(str(qqID.result)), int(str(days.result)), time.time())
         else:
             await addBanList(
-                int(str(id.result)),
+                int(str(qqID.result)),
                 int(str(days.result)),
                 time.time(),
                 ban_tip=str(tip.result),
             )
-        logger.info(f"成功封禁{str(id.result)} {str(days.result)}天,封禁信息:{str(tip.result)} ")
+        logger.info(
+            f"成功封禁{str(qqID.result)} {str(days.result)}天,封禁信息:{str(tip.result)} "
+        )
